25ea307110d34e23e18138b919010959db4c10b1
Shreyas-Ramesh10 <shreyas133719@gmail.com>
2025-08-19 15:38:18 +0530

    Added Diffuse light

--- OpenGL_World/main.cpp
+++ OpenGL_World/main.cpp
@@ -52,6 +52,40 @@ static const char* vShader = "Shaders/shader_vert.glsl.txt";
 // Fragment Shader
 static const char* fShader = "Shaders/shader_frag.glsl.txt";
 
+void calcAverageNormals(unsigned int * indices, unsigned int indiceCount, GLfloat * vertices, unsigned int verticeCount, 
+	unsigned int vLength, unsigned int normalOffset) 
+{
+	for (size_t i = 0; i < indiceCount; i += 3) 
+	{
+		unsigned int in0 = indices[i] * vLength;
+		unsigned int in1 = indices[i + 1] * vLength;
+		unsigned int in2 = indices[i + 2] * vLength;
+		glm::vec3 v1(vertices[in2] - vertices[in0], vertices[in1 + 1] - vertices[in0 + 1], vertices[in1 + 2] - vertices[in0 + 2]);
+		glm::vec3 v2(vertices[in2] - vertices[in0], vertices[in2 + 1] - vertices[in0 + 1], vertices[in2 + 2] - vertices[in0 + 2]);
+		glm::vec3 normal = glm::cross(v1, v2); // /\ Normal will give the line poking out of the triangle, either towards us or away from us.
+		normal = glm::normalize(normal);
+
+		in0 += normalOffset; in1 += normalOffset; in2 += normalOffset;
+		vertices[in0] += normal.x;
+		vertices[in0 + 1] += normal.y;
+		vertices[in0 + 2] += normal.z;
+
+		vertices[in1] += normal.x;
+		vertices[in1 + 1] += normal.y;
+		vertices[in1 + 2] += normal.z;
+
+		vertices[in2] += normal.x;
+		vertices[in2 + 1] += normal.y;
+		vertices[in2 + 2] += normal.z;
+	}
+	for (size_t i = 0; i < verticeCount / vLength; i++) 
+	{
+		unsigned int normOffset = i * vLength + normalOffset;
+		glm::vec3 vec(vertices[normOffset], vertices[normOffset + 1], vertices[normOffset + 2]);
+		vec = glm::normalize(vec);
+		vertices[normOffset] = vec.x; vertices[normOffset + 1] = vec.y; vertices[normOffset + 2] = vec.z;
+	}
+};
 
 void CreateObjects()
 {
@@ -62,19 +96,21 @@ void CreateObjects()
 		0, 1, 2,
 	};
 	GLfloat vertices[] = {
-	//    x      y     z      u     v (height)
-		-1.0f, -1.0f, 0.0f,  0.0f, 0.0f, // Bottom Left
-		 0.0f, -1.0f, 1.0f,	 0.5f, 0.0f, // Background
-		 1.0f, -1.0f, 0.0f,  1.0f, 0.0f, // Bottom Right
-		 0.0f,  1.0f, 0.0f,  0.5f, 1.0f, // Top
+	//    x      y     z      u     v (height)  normX normY normZ
+		-1.0f, -1.0f, 0.0f,  0.0f, 0.0f,        0.0f, 0.0f, 0.0f,// Bottom Left
+		 0.0f, -1.0f, 1.0f,	 0.5f, 0.0f,		0.0f, 0.0f, 0.0f,// Background
+		 1.0f, -1.0f, 0.0f,  1.0f, 0.0f,		0.0f, 0.0f, 0.0f,// Bottom Right
+		 0.0f,  1.0f, 0.0f,  0.5f, 1.0f,		0.0f, 0.0f, 0.0f // Top
 	};
 
+	calcAverageNormals(indices, 12, vertices, 32, 8, 5);
+
 	Mesh* obj1 = new Mesh();
-	obj1->CreateMesh(vertices, indices, 20, 12);
+	obj1->CreateMesh(vertices, indices, 32, 12);
 	meshList.push_back(obj1);
 
 	Mesh* obj2 = new Mesh();
-	obj2->CreateMesh(vertices, indices, 20, 12);
+	obj2->CreateMesh(vertices, indices, 32, 12);
 	meshList.push_back(obj2);
 }
  
@@ -105,7 +141,8 @@ int main()
 
 	brickTexture.UseTexture();
 
-	mainLight = Light(1.0f, 1.0f,1.0f,0.2f);
+	mainLight = Light(1.0f, 1.0f,1.0f,0.2f, 
+					2.0f, -1.0f, -2.0f, 1.0f);
 
 	/*GLint MaxUniforms;
 	glGetIntegerv(GL_MAX_VERTEX_UNIFORM_COMPONENTS, &MaxUniforms);
@@ -115,7 +152,7 @@ int main()
 	glGetIntegerv(GL_MAX_TEXTURE_IMAGE_UNITS, &maxTextureUnits);
 	printf("Max texture units in fragment shader: %d\n", maxTextureUnits);*/
 
-	GLuint uniformProjection = 0, uniformModel = 0, uniformView = 0, uniformAmbientIntensity = 0, uniformAmbientColor = 0;
+	GLuint uniformProjection = 0, uniformModel = 0, uniformView = 0, uniformAmbientIntensity = 0, uniformAmbientColor = 0, uniformDirection = 0 ,uniformDiffuseIntensity = 0;
 	glm::mat4 projection = glm::perspective(45.0f, (GLfloat)mainWindow2.getBufferWidth() / (GLfloat)mainWindow2.getBufferHeight(), 0.1f, 100.0f);
 
 	//Loop it until the window closes
@@ -143,8 +180,10 @@ int main()
 		uniformView = shaderList[0].GetViewLocation();
 		uniformAmbientColor = shaderList[0].GetAmbientColorLocation();
 		uniformAmbientIntensity = shaderList[0].GetAmbientIntensityLocation();
+		uniformDirection = shaderList[0].GetDirectionLocation();
+		uniformDiffuseIntensity = shaderList[0].GetDiffuseIntensityLocation();
 
-		mainLight.UseLight(uniformAmbientIntensity, uniformAmbientColor);
+		mainLight.UseLight(uniformAmbientIntensity, uniformAmbientColor, uniformDiffuseIntensity, uniformDirection);
 
 		glm::mat4 model(1.0f);//Will set "model" as an identity matrix
 
